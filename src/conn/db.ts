9608af9cbbd26ca418adda05aaf28c6dfaf2fb9a
Rodolfo Ferreira <rodolfo@speedcast.com.br>
2025-06-13 20:48:18 -0300

    initial DB

---
+++ src/conn/db.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('biblioteca', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+ export default sequelize;
