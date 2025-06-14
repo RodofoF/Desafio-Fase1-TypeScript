ddb3a2b1695226820500df6ff6512b042471c64f
Rodolfo Ferreira <rodolfo@speedcast.com.br>
2025-06-14 00:27:42 -0300

    update CRUD

--- src/controllers/livroController.ts
+++ src/controllers/livroController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Livro from "../models/Livro";
 import Editora from "../models/Editora";
 
-async function listarLivros(req:Request, res: Response) {
+async function getAllBooks(req:Request, res: Response) {
     try {
         const livros = await Livro.findAll({
             include:[{model: Editora, as: 'editora'}]
@@ -14,7 +14,7 @@ async function listarLivros(req:Request, res: Response) {
     }
 }
 
-async function criarLivro(req: Request, res: Response){
+async function postBook(req: Request, res: Response){
     try {
         const {titulo, autor, ano, editoraId} = req.body;
         const editora = await Editora.findByPk(editoraId)
@@ -28,7 +28,35 @@ async function criarLivro(req: Request, res: Response){
         res.status(500).json({error: 'Erro ao listar livros'})
     }
 }
+    async function deleteBookById(req: Request, res: Response){
+        try {
+            const livro = await Livro.findByPk(req.params.id)
+            if(!livro){
+                return res.status(404).json({message: "Book not found."})
+            }
+            await livro.destroy()
+            res.status(200).json({message: "The book has been deleted!"})
+        } catch (error) {
+            console.error('There was an error when trying to delete the book', error);
+            res.status(500).json({message: "Internal server error"})
+        }
+    }
+    async function updateBookById(req: Request, res: Response){
+        try {
+            const livro = await Livro.findByPk(req.params.id)
+            if(!livro){
+                return res.status(404).json({message: "Book not found."})
+            }
+            await livro.update(req.body)
+            res.status(200).json(req.body)
+        } catch (error) {
+             console.error('There were a error when you tryed update this book', error)
+            res.status(500).json({message: "Internal server error"})
+        }
+    }
 export default {
-    listarLivros,
-    criarLivro
+    getAllBooks,
+    postBook,
+    deleteBookById,
+    updateBookById
 }
--- src/routes/livros.ts
+++ src/routes/livros.ts
@@ -2,7 +2,9 @@ import { Router } from 'express'
 import livroController from '../controllers/livroController'
 const router = Router()
 
-router.get('/listar', livroController.listarLivros)
-router.post('/criar', livroController.criarLivro)
+router.get('/listar', livroController.getAllBooks)
+router.post('/criar', livroController.postBook)
+router.put('/editar/:id', livroController.updateBookById)
+router.delete('/apagar/:id', livroController.deleteBookById)
 
 export default router;
